d8deb0f78167dfd12f3037414f59cc247ce2f5ab
AsthaMakwana <asthamakwana2003@gmail.com>
2024-12-18 11:20:40 +0530

    redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 import authReducer from './authSlice';
 import studentReducer from './studentSlice';
 
-const store = configureStore({
-  reducer: {
-    students: studentReducer,
-    auth: authReducer
-  }
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  students: studentReducer,
+  auth: authReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
