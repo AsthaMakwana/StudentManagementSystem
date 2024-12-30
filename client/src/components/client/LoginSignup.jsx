6b95756fbbf0ab0bb2facafa78905d6f083b43b8
AsthaMakwana <asthamakwana2003@gmail.com>
2024-12-30 14:27:33 +0530

    login page validations

--- client/src/components/client/LoginSignup.jsx
+++ client/src/components/client/LoginSignup.jsx
@@ -4,16 +4,15 @@ import { observer } from 'mobx-react-lite';
 import authStore from '../../mobx/authStore';
 import { toast, ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import Joi from 'joi';
 import 'react-toastify/dist/ReactToastify.css';
 
 const LoginSignup = () => {
-
   const [isLoginMode, setIsLoginMode] = useState(true);
-  const [loginData, setLoginData] = useState({ email: '', password: '' });
-  const [signupData, setSignupData] = useState({ username: '', email: '', password: '' });
-  const [error, setError] = useState('');
-
   const navigate = useNavigate();
+
+  const { register, handleSubmit, setError, reset, formState: { errors }, setValue } = useForm();
 
   const [isNavigated, setIsNavigated] = useState(false);
 
@@ -25,94 +24,137 @@ const LoginSignup = () => {
     }
   }, [isNavigated, navigate]);
 
-  const handleLoginChange = (e) => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  const loginSchema = Joi.object({
+    email: Joi.string().email({ tlds: { allow: false } }).required().messages({ 'string.email': 'Invalid email', 'string.empty': 'Email is required' }),
+    password: Joi.string().min(6).required().messages({ 'string.min': 'Password must be at least 6 characters', 'string.empty': 'Password is required' }),
+  });
+
+  const signupSchema = Joi.object({
+    username: Joi.string().min(3).required().messages({ 'string.min': 'Username must be at least 3 characters', 'string.empty': 'Username is required' }),
+    email: Joi.string().email({ tlds: { allow: false } }).required().messages({ 'string.email': 'Invalid email', 'string.empty': 'Email is required' }),
+    password: Joi.string().min(6).required().messages({ 'string.min': 'Password must be at least 6 characters', 'string.empty': 'Password is required' }),
+    role: Joi.string().valid('user', 'admin').required().messages({ 'any.only': 'Invalid role', 'string.empty': 'Role is required' }),
+  });
+
+  const validateForm = (data, schema) => {
+    const { error } = schema.validate(data, { abortEarly: false });
+    if (error) {
+      error.details.forEach((detail) => {
+        setError(detail.path[0], { type: 'manual', message: detail.message });
+      });
+      return false;
+    }
+    return true;
   };
 
-  const handleSignupChange = (e) => {
-    setSignupData({ ...signupData, [e.target.name]: e.target.value });
-  };
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  const handleLogin = async (data) => {
+    if (!validateForm(data, loginSchema)) return;
     try {
-      await authStore.login(loginData.email, loginData.password);
+      await authStore.login(data.email, data.password);
       if (authStore.status === 'succeeded') {
         toast.success('Login successful!');
         setTimeout(() => {
           navigate('/');
         }, 1000);
       } else {
-        setError(authStore.error);
+        toast.error(authStore.error.error)
       }
-    } catch (err) {
-      setError('Login failed. Please check your credentials.');
+    } catch (error) {
+      const errorMessage = error.response?.data?.error || 'Error during login. Please try again later.';
+      setError('email', { type: 'manual', message: errorMessage });
+      setError('password', { type: 'manual', message: errorMessage });
     }
   };
 
-  const handleSignup = async (e) => {
-    e.preventDefault();
+  const handleSignup = async (data) => {
+    if (!validateForm(data, signupSchema)) return;
     try {
-      await authStore.signup(signupData.username, signupData.email, signupData.password, signupData.role);
+      await authStore.signup(data.username, data.email, data.password, data.role);
       if (authStore.status === 'succeeded') {
         toast.success('Signup successful!');
         setTimeout(() => {
           navigate('/');
         }, 1000);
       } else {
-        setError(authStore.error);
+        toast.error(authStore.error.error)
       }
-    } catch (err) {
-      setError('Signup failed. Please try again.');
+    } catch (error) {
+      const errorMessage = error.response?.data?.error || 'User/Admin already exists';
+      if (errorMessage.includes('username')) {
+        setError('username', { type: 'manual', message: errorMessage });
+      } else if (errorMessage.includes('email')) {
+        setError('email', { type: 'manual', message: errorMessage });
+      } else {
+        toast.error(errorMessage);
+      }
     }
   };
 
+  const switchMode = () => {
+    setIsLoginMode((prevMode) => !prevMode);
+    reset();
+    setValue('password', '');
+  };
+
   return (
-
     <div className="container w-50">
-      <div className={`login form ${isLoginMode ? 'active' : ''}`}>
-        <header>Login</header>
-
-        <form onSubmit={handleLogin}>
-          <input type="email" name="email" placeholder="Enter your email" value={loginData.email} onChange={handleLoginChange} required />
-          <input type="password" name="password" placeholder="Enter your password" value={loginData.password} onChange={handleLoginChange} required />
-          {error && <div className="error">{error}</div>}
-          <input type="submit" className="button" value="Login" />
-        </form>
-
-        <div className="signup">
-          <span className="signup">
-            Don't have an account?
-            <label onClick={() => setIsLoginMode(false)}>Signup</label>
-          </span>
-        </div>
-      </div>
-
-      <div className={`registration form ${!isLoginMode ? 'active' : ''}`}>
-        <header>Signup</header>
-
-        <form onSubmit={handleSignup}>
-          <input type="text" name="username" placeholder="Enter your username" value={signupData.username} onChange={handleSignupChange} required />
-          <input type="email" name="email" placeholder="Enter your email" value={signupData.email} onChange={handleSignupChange} required />
-          <input type="password" name="password" placeholder="Create a password" value={signupData.password} onChange={handleSignupChange} required />
-          <select name="role" value={signupData.role} onChange={handleSignupChange} required>
-            <option value="user">User</option>
-            <option value="admin">Admin</option>
-          </select>
-          {error && <div className="error">{error}</div>}
-          <input type="submit" className="button" value="Signup" />
-        </form>
-
-        <div className="signup">
-          <span className="signup">
-            Already have an account?
-            <label onClick={() => setIsLoginMode(true)}>Login</label>
-          </span>
-        </div>
-      </div>
       <ToastContainer />
+
+      {isLoginMode ? (
+        <div className="login form active">
+          <header>Login</header>
+
+          <form onSubmit={handleSubmit(handleLogin)}>
+
+            <input type="email" placeholder="Enter your email" {...register('email')} className={errors.email ? 'error' : ''}/>
+            {errors.email && <div className="error">{errors.email.message}</div>}
+
+            <input type="password" placeholder="Enter your password" {...register('password')} className={errors.password ? 'error' : ''}/>
+            {errors.password && <div className="error">{errors.password.message}</div>}
+
+            <input type="submit" className="button" value="Login" />
+          </form>
+
+          <div className="signup">
+            <span>
+              Don't have an account? <label onClick={switchMode}>Signup</label>
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="registration form active">
+          <header>Signup</header>
+
+          <form onSubmit={handleSubmit(handleSignup)}>
+
+            <input type="text" placeholder="Enter your username" {...register('username')} className={errors.username ? 'error' : ''}/>
+            {errors.username && <div className="error">{errors.username.message}</div>}
+            
+            <input type="email" placeholder="Enter your email" {...register('email')} className={errors.email ? 'error' : ''}/>
+            {errors.email && <div className="error">{errors.email.message}</div>}
+
+            <input type="password" placeholder="Create a password" {...register('password')} className={errors.password ? 'error' : ''}/>
+            {errors.password && <div className="error">{errors.password.message}</div>}
+            
+            <select {...register('role')} className={errors.role ? 'error' : ''}>
+              <option>Select Role</option>
+              <option value="user">User</option>
+              <option value="admin">Admin</option>
+            </select>
+            {errors.role && <div className="error">{errors.role.message}</div>}
+
+            <input type="submit" className="button" value="Signup" />
+          </form>
+          
+          <div className="signup">
+            <span>
+              Already have an account? <label onClick={switchMode}>Login</label>
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-export default LoginSignup;
+export default observer(LoginSignup);
